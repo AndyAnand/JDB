c6cbf05576231d480ac6325f03181a04ca83f9a0
andy <andygajadhar@gmail.com>
2014-03-16 12:26:36 -0300

    primeiro

---
+++ index.php
@@ -0,0 +1,24 @@
+
+<!DOCTYPE html>
+<html lang="en">
+	<?php include('header.php'); ?>
+    <!-- Custom styles for this template -->
+    <link href="css/starter-template.css" rel="stylesheet">
+    <body>
+    <div class="container">
+
+      <div class="starter-template">
+        <h1>Seja Bem Vindo ao Jogo do Bicho!</h1>
+        <p class="lead">Faça já sua aposta! Basta fazer login e boa sorte!.</p>
+      </div>
+
+    </div><!-- /.container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="../../dist/js/bootstrap.min.js"></script>
+  </body>
+</html>
---
+++ login.php
@@ -0,0 +1,25 @@
+
+<!DOCTYPE html>
+<html lang="en">
+    <?php include('header.php'); ?>
+	<!-- Custom styles for this template -->
+    <link href="css/signin.css" rel="stylesheet">
+  <body>
+    
+    <div class="container">
+
+      <form class="form-signin" role="form">
+        <h3 class="form-signin-heading"><center>Por favor, faça seu login:</center></h2>
+        <input type="email" class="form-control" placeholder="Seu Endereço de Email" required autofocus>
+        <input type="password" class="form-control" placeholder="Sua Senha" required>
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Fazer Login</button>
+      </form>
+
+    </div> <!-- /container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+  </body>
+</html>
